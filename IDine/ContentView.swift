27f418061af242de9ad3e7d02f6fa1f192436738
thai-ru <robinthairu34@gmail.com>
2024-04-16 23:28:44 +0300

    Initial Commit

---
+++ IDine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IDine
+//
+//  Created by Robinson Thairu on 16/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IDine/IDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDineApp.swift
+//  IDine
+//
+//  Created by Robinson Thairu on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct IDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
